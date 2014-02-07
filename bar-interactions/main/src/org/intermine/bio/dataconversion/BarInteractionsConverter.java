cfe89d0a76baba00c445eadcb855ed0a5ccaa5de
Vivek Krishnakumar <vkrishnakumar@jcvi.org>
2014-02-07 15:21:01 -0500

    Added JBrowse displayer to report page, fixed the OverlappingFeatures box to appear similar to Flymine

--- bar-interactions/main/src/org/intermine/bio/dataconversion/BarInteractionsConverter.java
+++ bar-interactions/main/src/org/intermine/bio/dataconversion/BarInteractionsConverter.java
@@ -25,7 +25,7 @@ import org.intermine.sql.Database;
 import org.intermine.xml.full.Item;
 
 /**
- * 
+ *
  * @author Julie Sullivan
  */
 public class BarInteractionsConverter extends BioDBConverter
@@ -55,7 +55,7 @@ public class BarInteractionsConverter extends BioDBConverter
     public void process() throws Exception {
 
     	Connection connection = null;
-    	
+
         if (getDatabase() == null) {
             // no Database when testing and no connection needed
             connection = null;
@@ -65,16 +65,16 @@ public class BarInteractionsConverter extends BioDBConverter
         }
         processQueryResults(connection);
     }
-    
-    private void processQueryResults(Connection connection)    	
+
+    private void processQueryResults(Connection connection)
         throws SQLException, ObjectStoreException {
         ResultSet res = runInteractionsQuery(connection);
-    	while (res.next()) {
-    		String gene1 = res.getString(1);
-    		String gene2 = res.getString(2);
-    		Integer quality = new Integer(res.getInt(3));
-    		Integer index = new Integer(res.getInt(4));
-    		// is this a confidence score?
+	while (res.next()) {
+		String gene1 = res.getString(1);
+		String gene2 = res.getString(2);
+		Integer quality = new Integer(res.getInt(3));
+		Integer index = new Integer(res.getInt(4));
+		// is this a confidence score?
     		Double pcc = new Double(res.getDouble(5));
     		String pubString = res.getString(6);
     		String interactionsDetectionMI = res.getString(7);
@@ -82,35 +82,35 @@ public class BarInteractionsConverter extends BioDBConverter
     		String interactionsTypeMI = res.getString(8);
 //    		String interactionsType = res.getString(10);
 
-    		// strings that represent the stored gene
-    		String geneRefId1 = getGene(gene1);
-    		String geneRefId2 = getGene(gene2);
-    		
-    		String interactionRefId = processInteraction(geneRefId1, geneRefId2, quality, index, 
-    				pcc, pubString, interactionsDetectionMI, interactionsTypeMI);
-    		processInteractionDetails(interactionRefId, geneRefId1, geneRefId2, pubString, 
-    				interactionsDetectionMI, interactionsTypeMI);
-    	}   
-    	res.close();
+		// strings that represent the stored gene
+		String geneRefId1 = getGene(gene1);
+		String geneRefId2 = getGene(gene2);
+
+		String interactionRefId = processInteraction(geneRefId1, geneRefId2, quality, index,
+				pcc, pubString, interactionsDetectionMI, interactionsTypeMI);
+		processInteractionDetails(interactionRefId, geneRefId1, geneRefId2, pubString,
+				interactionsDetectionMI, interactionsTypeMI);
+	}
+	res.close();
     }
-    
-    private String processInteraction(String geneRefId1, String geneRefId2, 
-    		Integer quality, Integer index, Double pcc, String pubString, 
-    		String interactionsDetectionMI, String interactionsTypeMI) throws ObjectStoreException {
-    	
-    	Item interaction = createItem("Interaction");
-    	interaction.setReference("gene1", geneRefId1);
-    	interaction.setReference("gene2", geneRefId2);
-    	store(interaction);
-    	return interaction.getIdentifier();
+
+    private String processInteraction(String geneRefId1, String geneRefId2,
+		Integer quality, Integer index, Double pcc, String pubString,
+		String interactionsDetectionMI, String interactionsTypeMI) throws ObjectStoreException {
+
+	Item interaction = createItem("Interaction");
+	interaction.setReference("gene1", geneRefId1);
+	interaction.setReference("gene2", geneRefId2);
+	store(interaction);
+	return interaction.getIdentifier();
     }
-    
-    private String processInteractionDetails(String interactionRefId, String geneRefId1, 
-    		String geneRefId2, String pubString, String 
-    		interactionsDetectionMI, String interactionsTypeMI) 
-    		throws ObjectStoreException {
-    	Item detail = createItem("InteractionDetail");
-    	detail.setAttribute("type", INTERACTION_TYPE);
+
+    private String processInteractionDetails(String interactionRefId, String geneRefId1,
+		String geneRefId2, String pubString, String
+		interactionsDetectionMI, String interactionsTypeMI)
+		throws ObjectStoreException {
+	Item detail = createItem("InteractionDetail");
+	detail.setAttribute("type", INTERACTION_TYPE);
     	if (StringUtils.isNotEmpty(interactionsTypeMI)) {
     		detail.setReference("relationshipType", getTerm(interactionsTypeMI));
     	}
@@ -124,35 +124,35 @@ public class BarInteractionsConverter extends BioDBConverter
         store(detail);
         return detail.getIdentifier();
     }
-    
-    private String getExperiment(String pubString, String interactionsDetectionMI) 
+
+    private String getExperiment(String pubString, String interactionsDetectionMI)
     		throws ObjectStoreException {
         Item experiment = createItem("InteractionExperiment");
         // some publications don't start with PubMed, what are those?
-        if (StringUtils.isNotEmpty(pubString) && pubString.startsWith(PUBMED_PREFIX) 
+        if (StringUtils.isNotEmpty(pubString) && pubString.startsWith(PUBMED_PREFIX)
         		&& pubString.length() >  PUBMED_PREFIX.length() + 1) {
         	String pubRefId = getPublication(pubString);
         	if (StringUtils.isNotEmpty(pubRefId)) {
         		experiment.setReference("publication", pubRefId);
-        	}
+		}
         }
         if (StringUtils.isNotEmpty(interactionsDetectionMI)) {
-        	experiment.addToCollection("interactionDetectionMethods", 
-        			getTerm(interactionsDetectionMI));
+		experiment.addToCollection("interactionDetectionMethods",
+				getTerm(interactionsDetectionMI));
         }
         store(experiment);
         return experiment.getIdentifier();
     }
-    
+
     private String getPublication(String pubString) throws ObjectStoreException {
         String pubMedId =  pubString.substring(PUBMED_PREFIX.length());
-    	
-    	// why do some look like this?
-    	// PubMed18849490                  +
-    	        
+
+	// why do some look like this?
+	// PubMed18849490                  +
+
         String pubRefId = publications.get(pubMedId);
         if (pubRefId != null) {
-        	return pubRefId;
+		return pubRefId;
         }
         Item publication = createItem("Publication");
         publication.setAttribute("pubMedId", pubMedId);
@@ -162,27 +162,27 @@ public class BarInteractionsConverter extends BioDBConverter
         return pubRefId;
     }
 
-    private String getGene(String identifier) 
+    private String getGene(String identifier)
     		throws ObjectStoreException {
     	String geneRefId = genes.get(identifier);
 
     	// we've already seen this gene, don't store again
-    	if (geneRefId != null) {
-    		return geneRefId;
-    	}
-    	
-    	// create new gene 
-    	Item gene = createItem("Gene");
-    	gene.setReference("organism", getOrganismItem(TAXON_ID));
-    	gene.setAttribute("primaryIdentifier", identifier);
-    
-    	// put in our map 
-    	genes.put(identifier, geneRefId);
-    	
-    	// store to database
-    	store(gene);
-    	
-    	return gene.getIdentifier();
+	if (geneRefId != null) {
+		return geneRefId;
+	}
+
+	// create new gene
+	Item gene = createItem("Gene");
+	gene.setReference("organism", getOrganismItem(TAXON_ID));
+	gene.setAttribute("primaryIdentifier", identifier);
+
+	// put in our map
+	genes.put(identifier, geneRefId);
+
+	// store to database
+	store(gene);
+
+	return gene.getIdentifier();
     }
 
     private String getTerm(String identifier) throws ObjectStoreException {
@@ -215,9 +215,9 @@ public class BarInteractionsConverter extends BioDBConverter
      */
     protected ResultSet runInteractionsQuery(Connection connection) throws SQLException {
         Statement stmt = connection.createStatement();
-    	String query = "select protein1, protein2, quality, index, pcc, bind_id, " +
-    			"interactions_detection_mi, interactions_detection, interactions_type_mi, " +
-    			"interactions_type from interactions;";
+	String query = "select protein1, protein2, quality, index, pcc, bind_id, " +
+			"interactions_detection_mi, interactions_detection, interactions_type_mi, " +
+			"interactions_type from interactions;";
         ResultSet res = stmt.executeQuery(query);
         return res;
     }
