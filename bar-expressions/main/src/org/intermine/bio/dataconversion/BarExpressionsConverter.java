9d20ebf07381a4c7633154c49e0b6841d2c772bc
sergio contrino <contrino@gmail.com>
2014-02-20 14:36:12 +0000

    loads now also samples and sample data

--- bar-expressions/main/src/org/intermine/bio/dataconversion/BarExpressionsConverter.java
+++ bar-expressions/main/src/org/intermine/bio/dataconversion/BarExpressionsConverter.java
@@ -48,6 +48,10 @@ public class BarExpressionsConverter extends BioDBConverter
     private Map<Integer, String> experimentLabMap = new HashMap<Integer, String>();
     //pi, item Id
     private Map<String, String> labIdRefMap = new HashMap<String, String>();
+    //barId, item Id
+    private Map<Integer, String> experimentIdRefMap = new HashMap<Integer, String>();
+    //barId, item Id
+    private Map<Integer, String> sampleIdRefMap = new HashMap<Integer, String>();
 
     /**
      * Construct a new BarExpressionsConverter.
@@ -74,28 +78,25 @@ public class BarExpressionsConverter extends BioDBConverter
             connection = getDatabase().getConnection();
         }
 
-        LOG.info("START");
-        processExperiment(connection);
-//        processSample(connection);
-
+        processExperiments(connection);
+        processSamples(connection);
+        processSampleData(connection);
     }
 
-    private void processExperiment(Connection connection)
+    private void processExperiments(Connection connection)
         throws SQLException, ObjectStoreException {
         ResultSet res = getExperiments(connection);
     	while (res.next()) {
     		Integer experimentBarId = new Integer(res.getInt(1));
-            LOG.info("WHILE: " + experimentBarId);
-
     		String title = res.getString(2);
     		String pi = res.getString(3);
     		String affiliation = res.getString(4);
     		String address = res.getString(5);
 //    		Integer sampleId = new Integer(res.getInt(6));
 
-
-    		String expressionRefId = processExperiment(experimentBarId, title,
+    		String experimentRefId = createExperiment(experimentBarId, title,
     				pi, affiliation, address);
+    		experimentIdRefMap.put(experimentBarId, experimentRefId);
 //    		processExpressionDetails(expressionRefId, geneRefId1, geneRefId2, pubString,
 //    				expressionsDetectionMI, expressionsTypeMI);
     	}
@@ -125,8 +126,45 @@ public class BarExpressionsConverter extends BioDBConverter
 
     }
 
+    private void processSamples(Connection connection)
+            throws SQLException, ObjectStoreException {
+            ResultSet res = getSamples(connection);
+        	while (res.next()) {
+        		Integer experimentBarId = new Integer(res.getInt(1));
+        		Integer sampleBarId = new Integer(res.getInt(2));
+        		String name = res.getString(3);
+        		String alias = res.getString(4);
+        		String description = res.getString(5);
+        		String control = res.getString(6);
+        		String replication = res.getString(7);
+        		String file = res.getString(8);
 
-    private String processExperiment(Integer experimentBarId, String title,
+        		String sampleRefId = createSample(experimentBarId,
+        				sampleBarId, name, alias, description, control,
+        				replication, file);
+        		sampleIdRefMap.put(sampleBarId, sampleRefId);
+        	}
+        	res.close();
+    }
+
+    private void processSampleData(Connection connection)
+            throws SQLException, ObjectStoreException {
+            ResultSet res = getSampleData(connection);
+        	while (res.next()) {
+        		Integer sampleBarId = new Integer(res.getInt(1));
+        		String probeSet = res.getString(2);
+        		Double signal = res.getDouble(3);
+        		String call = res.getString(4);
+        		Double pValue = res.getDouble(5);
+
+        		String sampleDataRefId = createSampleData(sampleBarId,
+        				probeSet, signal, call, pValue);
+        	}
+        	res.close();
+    }
+
+
+    private String createExperiment(Integer experimentBarId, String title,
     		String pi, String affiliation, String address)
     				throws ObjectStoreException {
     	LOG.info("PROCEXP");
@@ -163,6 +201,116 @@ public class BarExpressionsConverter extends BioDBConverter
 		store(lab);
 		return lab.getIdentifier();
 	}
+
+    private String createSample(Integer experimentBarId, Integer sampleBarId,
+    		String name, String alias, String description,
+    		String control, String replication, String file)
+    				throws ObjectStoreException {
+    	Item sample = createItem("Sample");
+    	sample.setAttribute("barId", sampleBarId.toString());
+    	sample.setAttribute("name", name);
+    	sample.setAttribute("alias", alias);
+    	sample.setAttribute("description", description);
+    	sample.setAttribute("control", control);
+    	sample.setAttribute("replication", replication);
+    	sample.setAttribute("file", file);
+
+		sample.setReference("experiment", experimentIdRefMap.get(experimentBarId));
+		store(sample);
+    	return sample.getIdentifier();
+    }
+
+    private String createSampleData(Integer sampleBarId,
+    		String probeSet, Double signal, String call, Double pValue)
+    				throws ObjectStoreException {
+
+    	// needed to compensate for missing experiments for some smples
+    	String sampleIdRef = sampleIdRefMap.get(sampleBarId);
+
+    	if (sampleIdRef == null) {
+    		LOG.warn("Orphaned sample: id=" + sampleBarId +
+    				". The eperiment for this sample is missing.");
+    		return "orphaned sample";
+    	}
+
+
+    	Item sampleData = createItem("SampleData");
+    	sampleData.setAttribute("probeSet", probeSet);
+    	sampleData.setAttribute("signal", signal.toString());
+    	if (call!=null) {
+    		sampleData.setAttribute("call", call);
+    	}
+    	sampleData.setAttribute("pValue", pValue.toString());
+
+		sampleData.setReference("sample", sampleIdRef);
+		store(sampleData);
+    	return sampleData.getIdentifier();
+    }
+
+
+    /**
+     * Return the expressions from the bar-expressions table
+     * This is a protected method so that it can be overridden for testing.
+     * @param connection the bar database connection
+     * @throws SQLException if there is a problem while querying
+     * @return the expressions
+     */
+    protected ResultSet getExperiments(Connection connection) throws SQLException {
+        Statement stmt = connection.createStatement();
+        LOG.info("PREQ");
+    	String query =
+    			"select p.proj_id, p.proj_res_area, p.proj_pi, p.proj_pi_inst, "
+    			+ " p.proj_pi_addr "
+    			+ " from proj_info p;";
+//		"select p.proj_id, p.proj_res_area, p.proj_pi, p.proj_pi_inst, "
+//		+ " p.proj_pi_addr, s.sample_id "
+//		+ " from proj_info p, sample_biosource_info s "
+//		+ " where p.proj_id=s.proj_id;";
+        ResultSet res = stmt.executeQuery(query);
+        LOG.info("POSTQ");
+        return res;
+    }
+
+    /**
+     * Return the expressions from the bar-expressions table
+     * This is a protected method so that it can be overridden for testing.
+     * @param connection the bar database connection
+     * @throws SQLException if there is a problem while querying
+     * @return the expressions
+     */
+    protected ResultSet getSamples(Connection connection) throws SQLException {
+        Statement stmt = connection.createStatement();
+        LOG.info("PREQs");
+    	String query =
+    	"SELECT p.proj_id, sb.sample_id, sb.sample_bio_name, sb.sample_alias, "
+    	+ "sg.sample_desc, sg.sample_ctrl, sg.sample_repl, sg.sample_file_name "
+    	+ "FROM sample_biosource_info sb, sample_general_info sg, proj_info p "
+    	+ "WHERE sb.sample_id=sg.sample_id "
+    	+ "AND p.proj_id=sb.proj_id;";
+    	ResultSet res = stmt.executeQuery(query);
+        LOG.info("POSTQS");
+        return res;
+    }
+
+    /**
+     * Return the expressions from the bar-expressions table
+     * This is a protected method so that it can be overridden for testing.
+     * @param connection the bar database connection
+     * @throws SQLException if there is a problem while querying
+     * @return the expressions
+     */
+    protected ResultSet getSampleData(Connection connection) throws SQLException {
+        Statement stmt = connection.createStatement();
+        LOG.info("PREQs");
+    	String query =
+    			"SELECT sample_id, data_probeset_id, data_signal, "
+    			+ "data_call, data_p_val "
+    			+ "FROM sample_data;";
+    	ResultSet res = stmt.executeQuery(query);
+        LOG.info("POSTQS");
+        return res;
+    }
+
 
 	/*
     private String processExpression(String geneRefId1, String geneRefId2,
@@ -279,26 +427,4 @@ public class BarExpressionsConverter extends BioDBConverter
         return DATA_SOURCE_NAME + " expressions data set";
     }
 
-    /**
-     * Return the expressions from the bar-expressions table
-     * This is a protected method so that it can be overridden for testing.
-     * @param connection the bar database connection
-     * @throws SQLException if there is a problem while querying
-     * @return the expressions
-     */
-    protected ResultSet getExperiments(Connection connection) throws SQLException {
-        Statement stmt = connection.createStatement();
-        LOG.info("PREQ");
-    	String query =
-    			"select p.proj_id, p.proj_res_area, p.proj_pi, p.proj_pi_inst, "
-    			+ " p.proj_pi_addr "
-    			+ " from proj_info p;";
-//		"select p.proj_id, p.proj_res_area, p.proj_pi, p.proj_pi_inst, "
-//		+ " p.proj_pi_addr, s.sample_id "
-//		+ " from proj_info p, sample_biosource_info s "
-//		+ " where p.proj_id=s.proj_id;";
-        ResultSet res = stmt.executeQuery(query);
-        LOG.info("POSTQ");
-        return res;
-    }
 }
