72d2fd07437805b6ab6ccf2c5a5ebbd9c931e60f
sergio contrino <contrino.it@gmail.com>
2016-04-22 10:11:57 +0100

    just cs

--- araport-chado-db/main/src/org/intermine/bio/dataloader/job/FlowStep.java
+++ araport-chado-db/main/src/org/intermine/bio/dataloader/job/FlowStep.java
@@ -1,5 +1,15 @@
 package org.intermine.bio.dataloader.job;
 
+/*
+ * Copyright (C) 2002-2015 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
 import org.apache.commons.lang.time.StopWatch;
 import org.apache.log4j.Logger;
 import org.intermine.bio.item.ItemProcessor;
@@ -7,138 +17,102 @@ import org.intermine.item.domain.database.DatabaseItemReader;
 import org.intermine.item.domain.database.ParseException;
 import org.intermine.item.domain.database.UnexpectedInputException;
 
-public class FlowStep<I,O> extends AbstractStep{
+public class FlowStep<I, O> extends AbstractStep
+{
+    protected static final Logger LOG = Logger.getLogger(FlowStep.class);
+    private static final StopWatch TIMER = new StopWatch();
+    private DatabaseItemReader<? extends I> reader;
+    private ItemProcessor<? super I, ? extends O> processor;
+    private Step postProcessor;
+    private TaskExecutor taskExecutor;
 
-	protected static final Logger log = Logger.getLogger(FlowStep.class);
-    private final static StopWatch timer = new StopWatch();
-    
-	private DatabaseItemReader<? extends I> reader;
-	
-	private ItemProcessor<? super I, ? extends O> processor;
-	
-	private Step postProcessor;
-	
-	private TaskExecutor taskExecutor;
-	
-	@Override
-	protected void open() throws Exception {
-		
-		if (reader !=null){
-			
-			log.info("Opening Cursor in FlowStep:" + this.getName());
-			
-			reader.open();
-			
-			log.info("Successfully Opened Cursor in FlowStep:" + this.getName());
+    @Override
+    protected void open() throws Exception {
+        if (reader != null) {
+            LOG.info("Opening Cursor in FlowStep:" + this.getName());
+            reader.open();
+            LOG.info("Successfully Opened Cursor in FlowStep:" + this.getName());
+        } else {
+            LOG.error("Error Opening Cursor in FlowStep");
+        }
+    }
 
-		}else{
-			log.error("Error Opening Cursor in FlowStep");
-		}
-	}
-	
-	@Override
-	protected void doExecute(StepExecution stepExecution) throws Exception {
-		
-		taskExecutor.execute(new Runnable(){
-			
-			public void run(){
-				log.info("Running Flow Step!" + getName());
-				
-				I item = null;
-				
-				try {
-					
-					while (reader.hasNext()) {
-						
-						item = reader.read();
-						log.info("SQL" + reader.getSql());
-						log.info("Current Item = " + item);
-						log.info("Parameter values:" + reader.getParameterMap());
-						
-						if (item!=null){
-							
-							log.info("Item will be created: " + item);
-							
-							O processedItem = processor.process(item);
-							
-							log.info("Item has been created: " + processedItem);
-							
-						}
-						
-					}
-					
-				} catch (UnexpectedInputException e1) {
-					log.error("Error: " + e1.getCause());
-					e1.printStackTrace();
-				} catch (ParseException e1) {
-					
-					log.error("Error:  " + e1.getCause());
-					
-				
-					e1.printStackTrace();
-				} catch (Exception e1) {
-					
-					log.error("Error: " + e1.getCause());
-					
-					e1.printStackTrace();
-				}
-				
-				log.info("Flow Step has been completed!");
-			}
-		});
-		
-		
-		
-	}
-	
-	@Override
-	protected void doPostProcess(StepExecution stepExecution) throws Exception {
-		
-		log.info("Evaluating PostProcessing!");
-		
-		if (postProcessor!=null){
-			log.info("Running PostProcessing!");
-			postProcessor.execute(stepExecution);
-		} else{
-			log.info("PostProcessor is Null");
-		}
-		
-		log.info("Evaluating PostProcessing!");
-	}
-	
-	public void setItemReader(DatabaseItemReader<? extends I> reader) {
-		this.reader = reader;
-	}
-	
-	public void setItemProcessor(ItemProcessor<? super I, ? extends O> processor) {
-		this.processor = processor;
-	}
-	
-	protected DatabaseItemReader<? extends I> getItemReader() {
-		return reader;
-	}
-	
-	protected ItemProcessor<? super I, ? extends O> getItemProcessor() {
-		return processor;
-	}
-	
-	public void setStepPostProcessor(Step postProcessor) {
-		this.postProcessor = postProcessor;
-	}
-	
+    @Override
+    protected void doExecute(StepExecution stepExecution) throws Exception {
+        taskExecutor.execute(new Runnable() {
+            public void run() {
+                LOG.info("Running Flow Step!" + getName());
+                I item = null;
+                try {
+                    while (reader.hasNext()) {
+                        item = reader.read();
+                        LOG.info("SQL" + reader.getSql());
+                        LOG.info("Current Item = " + item);
+                        LOG.info("Parameter values:" + reader.getParameterMap());
+                        if (item != null) {
+                            LOG.info("Item will be created: " + item);
+                            O processedItem = processor.process(item);
+                            LOG.info("Item has been created: " + processedItem);
+                        }
+                    }
+                } catch (UnexpectedInputException e1) {
+                    LOG.error("Error: " + e1.getCause());
+                    e1.printStackTrace();
+                } catch (ParseException e1) {
+                    LOG.error("Error:  " + e1.getCause());
+                    e1.printStackTrace();
+                } catch (Exception e1) {
+                    LOG.error("Error: " + e1.getCause());
+                    e1.printStackTrace();
+                }
+                LOG.info("Flow Step has been completed!");
+            }
+        });
+    }
 
-	protected Step getStepProcessor() {
-		return postProcessor;
-	}
-	
-	
-	public void setTaskExecutor(TaskExecutor taskExecutor) {
-		this.taskExecutor = taskExecutor;
-	}
-	
-	protected TaskExecutor getTaskExecutor() {
-		return taskExecutor;
-	}
+    @Override
+    protected void doPostProcess(StepExecution stepExecution) throws Exception {
+        LOG.info("Evaluating PostProcessing!");
+        if (postProcessor != null) {
+            LOG.info("Running PostProcessing!");
+            postProcessor.execute(stepExecution);
+        } else {
+            LOG.info("PostProcessor is Null");
+        }
+        LOG.info("Evaluating PostProcessing!");
+    }
 
-	
+    public void setItemReader(DatabaseItemReader<? extends I> reader) {
+        this.reader = reader;
+    }
+
+    public void setItemProcessor(ItemProcessor<? super I, ? extends O> processor) {
+        this.processor = processor;
+    }
+
+    protected DatabaseItemReader<? extends I> getItemReader() {
+        return reader;
+    }
+
+    protected ItemProcessor<? super I, ? extends O> getItemProcessor() {
+        return processor;
+    }
+
+    public void setStepPostProcessor(Step postProcessor) {
+        this.postProcessor = postProcessor;
+    }
+
+
+    protected Step getStepProcessor() {
+        return postProcessor;
+    }
+
+
+    public void setTaskExecutor(TaskExecutor taskExecutor) {
+        this.taskExecutor = taskExecutor;
+    }
+
+    protected TaskExecutor getTaskExecutor() {
+        return taskExecutor;
+    }
 }
