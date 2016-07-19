13746e9283d176c7018e3afa3f94304811ff3fcd
sergio contrino <contrino.it@gmail.com>
2016-07-19 17:07:26 +0100

    cs and clean

--- araport-chado-db/main/src/org/intermine/bio/item/postprocessor/BackgroundAccessionStockItemPostprocessor.java
+++ araport-chado-db/main/src/org/intermine/bio/item/postprocessor/BackgroundAccessionStockItemPostprocessor.java
@@ -6,14 +6,11 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
-import org.intermine.bio.chado.CVService;
 import org.intermine.bio.chado.OrganismService;
 import org.intermine.bio.dataconversion.ChadoDBConverter;
 import org.intermine.bio.dataloader.job.AbstractStep;
 import org.intermine.bio.dataloader.job.StepExecution;
 import org.intermine.bio.dataloader.job.TaskExecutor;
-import org.intermine.bio.dataloader.job.TaskletStep;
-import org.intermine.bio.domain.source.SourceCVTerm;
 import org.intermine.bio.item.util.ItemHolder;
 import org.intermine.bio.store.service.StoreService;
 import org.intermine.objectstore.ObjectStoreException;
@@ -22,162 +19,116 @@ import org.intermine.xml.full.ReferenceList;
 
 public class BackgroundAccessionStockItemPostprocessor extends AbstractStep {
 
-	protected static final Logger log = Logger.getLogger(BackgroundAccessionStockItemPostprocessor.class);
+    protected static final Logger LOG =
+            Logger.getLogger(BackgroundAccessionStockItemPostprocessor.class);
 
-	private static ChadoDBConverter service;
+    private static ChadoDBConverter service;
+    protected TaskExecutor taskExecutor;
 
-	protected TaskExecutor taskExecutor;
+    public BackgroundAccessionStockItemPostprocessor(ChadoDBConverter chadoDBConverter) {
+        super();
+        service = chadoDBConverter;
+    }
 
-	public BackgroundAccessionStockItemPostprocessor(ChadoDBConverter chadoDBConverter) {
-		super();
-		service = chadoDBConverter;
+    public BackgroundAccessionStockItemPostprocessor getPostProcessor(
+            String name, ChadoDBConverter chadoDBConverter, TaskExecutor taskExecutor) {
 
-	}
+        BackgroundAccessionStockItemPostprocessor processor =
+                new BackgroundAccessionStockItemPostprocessor( chadoDBConverter);
+        processor.setName(name);
+        processor.setTaskExecutor(taskExecutor);
+        return processor;
+    }
 
-	public BackgroundAccessionStockItemPostprocessor getPostProcessor(String name, ChadoDBConverter chadoDBConverter,
-			TaskExecutor taskExecutor
+    @Override
+    protected void doExecute(StepExecution stepExecution) throws Exception {
+        taskExecutor.execute(new Runnable() {
+            public void run() {
+                createStockBackgroundAccessions();
+            }
+        });
+    }
 
-	) {
+    @Override
+    protected void doPostProcess(StepExecution stepExecution) throws Exception {
+        // TODO Auto-generated method stub
+    }
 
-		BackgroundAccessionStockItemPostprocessor processor = new BackgroundAccessionStockItemPostprocessor(
-				chadoDBConverter);
-		processor.setName(name);
+    public void setTaskExecutor(TaskExecutor taskExecutor) {
+        this.taskExecutor = taskExecutor;
+    }
 
-		processor.setTaskExecutor(taskExecutor);
+    protected TaskExecutor getTaskExecutor() {
+        return taskExecutor;
+    }
 
-		return processor;
+    // not used, to rm?
+    private void createStockStrainCollection() {
+        Map<String, Item> items = OrganismService.getStrainItemSet();
+        LOG.debug("Total Count of Entities to Process:" + items.size());
+        for (Map.Entry<String, Item> item : items.entrySet()) {
+            Exception exception = null;
+            ItemHolder itemHolder = null;
+            Item strainItem = null;
 
-	}
+            String strain = item.getKey();
+            LOG.info("Processing BackGround Strain/Stock Collection: " + strain);
+            Collection<Item> collection = (Collection<Item>) item.getValue();
 
-	@Override
-	protected void doExecute(StepExecution stepExecution) throws Exception {
+            List terms = new ArrayList(collection);
+            itemHolder = OrganismService.getStrainMap().get(strain);
+            try {
+                if (itemHolder == null) {
+                    exception = new Exception("Strain " + strain + " not in organism service!");
+                    throw exception;
+                }
 
-		log.info("Running Task Let Step!  BackgroundAccessionStockItemPostprocessor: " + getName());
+                strainItem = itemHolder.getItem();
+                if (strainItem == null) {
+                    exception = new Exception("Strain " + strain + " gives null item!");
+                    throw exception;
+                }
 
-		taskExecutor.execute(new Runnable() {
+                ReferenceList referenceList = new ReferenceList();
+                referenceList.setName("stocks");
+                StoreService.storeCollection(collection, itemHolder, referenceList.getName());
+            } catch (ObjectStoreException e) {
+                exception  = e;
+            } catch (Exception e) {
+                exception  = e;
+            } finally {
+                if (exception  != null) {
+                    LOG.error("Strain:" + strain + " failed: "
+                            + exception .getMessage());
+                } else {
+                    LOG.debug("Stock/Background Strain Collection successfully stored."
+                            + itemHolder.getItem() + " Collection size:" + collection.size());
+                }
+            }
+        }
+    }
 
-			public void run() {
-				createStockBackgroundAccessions();
-			}
-		});
+    private void createStockBackgroundAccessions() {
+        Map<String, Item> items = OrganismService.getBgStrainItemSet();
+        for (Map.Entry<String, Item> item : items.entrySet()) {
+            String strain = item.getKey();
+            LOG.info("Processing Background Accession Strain " + strain);
+            Collection<Item> collection = (Collection<Item>) item.getValue();
+            Item strainItem = OrganismService.getStrainMap().get(strain).getItem();
+            ItemHolder itemHolder = OrganismService.getStrainMap().get(strain);
 
-	}
+            ReferenceList referenceList = new ReferenceList();
+            referenceList.setName("backgroundStocks");
 
-	@Override
-	protected void doPostProcess(StepExecution stepExecution) throws Exception {
-		// TODO Auto-generated method stub
+            try {
+                StoreService.storeCollection(collection, itemHolder, referenceList.getName());
+                LOG.debug("Collection successfully stored." + itemHolder.getItem() + ";"
+                        + "Collection size:" + collection.size());
+            } catch (ObjectStoreException e) {
+                LOG.error("Error storing back ground stocks collection for strain:" + strain);
+            }
+        }
 
-	}
-
-	public void setTaskExecutor(TaskExecutor taskExecutor) {
-		this.taskExecutor = taskExecutor;
-	}
-
-	protected TaskExecutor getTaskExecutor() {
-		return taskExecutor;
-	}
-
-	private void createStockStrainCollection() {
-		Map<String, Item> items = OrganismService.getStrainItemSet();
-		
-		log.debug("Total Count of Entities to Process:" + items.size());
-		
-				
-		for (Map.Entry<String, Item> item : items.entrySet()) {
-
-			Exception exception = null;
-			ItemHolder itemHolder = null;
-			Item strainItem = null;
-			
-			String strain = item.getKey();
-
-			log.info("Processing BackGround Strain/Stock Collection: " + strain);
-
-			Collection<Item> collection = (Collection<Item>) item.getValue();
-
-			List terms = new ArrayList(collection);
-
-			itemHolder = OrganismService.getStrainMap().get(strain);
-			
-			try {
-				
-			if (itemHolder==null){
-				exception = new Exception("StrainItemHolder is Null! Source Record does not exists in the Service Lookup!");
-				throw exception;
-			}
-						
-			strainItem = itemHolder.getItem();
-			
-			if (strainItem==null){
-				exception = new Exception("StrainItem is Null! Source Record does not exists in the Service Lookup!");
-				throw exception;
-			}
-			
-
-			log.debug("Total Count of Entities to Process:" + collection.size());
-							
-			ReferenceList referenceList = new ReferenceList();
-			referenceList.setName("stocks");
-
-			StoreService.storeCollection(collection, itemHolder, referenceList.getName());
-			
-			} catch (ObjectStoreException e) {
-				exception  = e;
-			} catch (Exception e) {
-				exception  = e;
-
-			} finally {
-				if (exception  != null) {
-					log.error("Error storing Stock/Background Strain Collection collection for strain:" + strain + "; Error:"
-							+ exception .getMessage());
-				} else {
-					log.debug("Stock/Background Strain Collection successfully stored." + itemHolder.getItem() + ";"
-							+ "Collection size:" + collection.size());
-				}
-			}
-
-		}
-
-		log.debug("Strain Map Item Size =" + items.size());
-
-		log.debug("Tasklet Task has Completed! " + getName());
-
-	}
-
-	private void createStockBackgroundAccessions() {
-
-		Map<String, Item> items = OrganismService.getBgStrainItemSet();
-
-		for (Map.Entry<String, Item> item : items.entrySet()) {
-
-			String strain = item.getKey();
-
-			log.info("Processing Background Accession Strain: " + strain);
-
-			Collection<Item> collection = (Collection<Item>) item.getValue();
-
-			Item strainItem = OrganismService.getStrainMap().get(strain).getItem();
-
-			log.info("Current Strain Item: " + strainItem);
-
-			ItemHolder itemHolder = OrganismService.getStrainMap().get(strain);
-
-			ReferenceList referenceList = new ReferenceList();
-			referenceList.setName("backgroundStocks");
-
-			try {
-
-				StoreService.storeCollection(collection, itemHolder, referenceList.getName());
-
-				log.info("Collection successfully stored." + referenceList.getName() + ";" + itemHolder.getItem() + ";"
-						+ "Collection size:" + collection.size());
-
-			} catch (ObjectStoreException e) {
-				log.error("Error storing back ground stocks collection for strain:" + strain);
-			}
-
-		}
-
-	}
+    }
 
 }
