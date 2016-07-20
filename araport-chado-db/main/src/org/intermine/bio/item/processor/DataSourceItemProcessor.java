19df771e9f6e9ad5b54f99e43583b5393845fc45
sergio contrino <contrino.it@gmail.com>
2016-07-20 14:06:41 +0100

    cs, cleans and corrects ecotype

--- araport-chado-db/main/src/org/intermine/bio/item/processor/DataSourceItemProcessor.java
+++ araport-chado-db/main/src/org/intermine/bio/item/processor/DataSourceItemProcessor.java
@@ -1,219 +1,171 @@
 package org.intermine.bio.item.processor;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
-import org.intermine.bio.chado.AlleleService;
-import org.intermine.bio.chado.CVService;
 import org.intermine.bio.chado.DataSetService;
 import org.intermine.bio.chado.DataSourceService;
-import org.intermine.bio.chado.GenotypeService;
-import org.intermine.bio.chado.OrganismService;
-import org.intermine.bio.chado.PhenotypeService;
-import org.intermine.bio.chado.PublicationService;
-import org.intermine.bio.chado.StockService;
 import org.intermine.bio.dataconversion.ChadoDBConverter;
 import org.intermine.bio.dataloader.job.AbstractStep;
 import org.intermine.bio.dataloader.job.StepExecution;
 import org.intermine.bio.dataloader.job.TaskExecutor;
-import org.intermine.bio.dataloader.job.TaskletStep;
-import org.intermine.bio.domain.source.SourceCVTerm;
 import org.intermine.bio.item.util.ItemHolder;
 import org.intermine.bio.store.service.StoreService;
 import org.intermine.objectstore.ObjectStoreException;
 import org.intermine.xml.full.Item;
-import org.intermine.xml.full.ReferenceList;
 
-public class DataSourceItemProcessor extends AbstractStep {
+public class DataSourceItemProcessor extends AbstractStep
+{
+    protected static final Logger LOG = Logger.getLogger(DataSourceItemProcessor.class);
+    private static ChadoDBConverter service;
+    protected TaskExecutor taskExecutor;
 
-	protected static final Logger log = Logger.getLogger(DataSourceItemProcessor.class);
+    public DataSourceItemProcessor(ChadoDBConverter chadoDBConverter) {
+        super();
+        service = chadoDBConverter;
+    }
 
-	private static ChadoDBConverter service;
+    public DataSourceItemProcessor getPostProcessor(String name, ChadoDBConverter chadoDBConverter,
+            TaskExecutor taskExecutor
+    ) {
+        DataSourceItemProcessor processor = new DataSourceItemProcessor(chadoDBConverter);
+        processor.setName(name);
+        processor.setTaskExecutor(taskExecutor);
+        return processor;
+    }
 
-	protected TaskExecutor taskExecutor;
+    @Override
+    protected void doExecute(StepExecution stepExecution) throws Exception {
+        LOG.info("Running Task " + getName());
+        taskExecutor.execute(new Runnable() {
+            public void run() {
+                createDataSources();
+            }
+        });
+    }
 
-	public DataSourceItemProcessor(ChadoDBConverter chadoDBConverter) {
-		super();
-		service = chadoDBConverter;
+    @Override
+    protected void doPostProcess(StepExecution stepExecution) throws Exception {
+        // TODO Auto-generated method stub
+    }
 
-	}
+    public void setTaskExecutor(TaskExecutor taskExecutor) {
+        this.taskExecutor = taskExecutor;
+    }
 
-	public DataSourceItemProcessor getPostProcessor(String name, ChadoDBConverter chadoDBConverter,
-			TaskExecutor taskExecutor
+    protected TaskExecutor getTaskExecutor() {
+        return taskExecutor;
+    }
 
-	) {
+    private void createDataSources() {
 
-		DataSourceItemProcessor processor = new DataSourceItemProcessor(chadoDBConverter);
-		processor.setName(name);
+        Item ncbiDataSourceItem = createDataSource("NCBI");
+        Item ncbiDataSet = createDataSet(ncbiDataSourceItem,
+                "PubMed to gene mapping", null, null, null);
 
-		processor.setTaskExecutor(taskExecutor);
+        Item tairDataSourceItem = createDataSource("TAIR");
+        String version = "TAIR Germplasm, October 2013";
+        String url = "https://www.arabidopsis.org/";
+        String description = "ABRC Germplasm/Seed Stock";
+        Item germplasmDataSet = createDataSet(tairDataSourceItem,
+                "TAIR Germplasm", version, url, description);
 
-		return processor;
 
-	}
+        String versionPoly = "TAIR Polymorphism, October 2013";
+        String urlPoly = "https://www.arabidopsis.org/";
+        String descriptionPoly = "TAIR Polymorphism";
+        Item polyDataSet = createDataSet(tairDataSourceItem,
+                "TAIR Polymorphism", versionPoly, urlPoly, descriptionPoly);
 
-	@Override
-	protected void doExecute(StepExecution stepExecution) throws Exception {
+        String versionPheno = "TAIR Phenotypes, October 2013";
+        String urlPheno = "https://www.arabidopsis.org/";
+        String descriptionPheno = "TAIR Phenotypes";
+        Item phenoDataSet = createDataSet(tairDataSourceItem,
+                "TAIR Phenotypes", versionPheno, urlPheno, descriptionPheno);
 
-		log.info("Running Task Let Step!  DataSource Item Processor " + getName());
+        String versionEcotypes = "TAIR Ecotypes, October 2013";
+        String urlEcotypes = "https://www.arabidopsis.org/";
+        String descriptionEcotypes = "TAIR Ecotypes";
+        Item ecotyTypesDataSet = createDataSet(tairDataSourceItem,
+                "TAIR Ecotypes", versionEcotypes, urlEcotypes, descriptionEcotypes);
+    }
 
-		taskExecutor.execute(new Runnable() {
+    private Item createDataSource(String dataSourceName) {
 
-			public void run() {
-				createDataSources();
-			}
-		});
+        Item dataSourceItem = null;
+        Exception exception = null;
+        ItemHolder itemHolder = null;
+        int itemId = -1;
 
-	}
+        try {
+            dataSourceItem = StoreService.getService().createItem("DataSource");
+            dataSourceItem.setAttribute("name", dataSourceName);
+            itemId = StoreService.getService().store(dataSourceItem);
+        } catch (ObjectStoreException e) {
+            exception = e;
+        } catch (Exception e) {
+            exception = e;
+        } finally {
 
-	@Override
-	protected void doPostProcess(StepExecution stepExecution) throws Exception {
-		// TODO Auto-generated method stub
+            if (exception != null) {
+                LOG.error("Error storing item for datasource record:" + dataSourceName);
+            } else {
+                LOG.info("Target Item has been created. DataSource:" + dataSourceItem);
 
-	}
+                itemHolder = new ItemHolder(dataSourceItem, itemId);
+                if (itemHolder != null && itemId != -1) {
+                    DataSourceService.addDataSourceItem(dataSourceName, itemHolder);
+                }
+            }
+        }
+        return dataSourceItem;
+    }
 
-	public void setTaskExecutor(TaskExecutor taskExecutor) {
-		this.taskExecutor = taskExecutor;
-	}
+    private Item createDataSet(Item dataSource, String dataSetName, String version, String url,
+            String description) {
 
-	protected TaskExecutor getTaskExecutor() {
-		return taskExecutor;
-	}
+        Item dataSetItem = null;
+        Exception exception = null;
+        ItemHolder itemHolder = null;
+        int itemId = -1;
 
-	private void createDataSources() {
+        if (dataSource != null && !StringUtils.isBlank(dataSetName)) {
+            String datasourceRefId = dataSource.getIdentifier();
 
-		Item ncbiDataSourceItem = createDataSource("NCBI");
-		Item ncbiDataSet = createDataSet(ncbiDataSourceItem, "PubMed to gene mapping", null, null, null);
-		
-		Item tairDataSourceItem = createDataSource("TAIR");
-		String version = "TAIR Germplasm, October 2013";
-		String url = "https://www.arabidopsis.org/";
-		String description = "ABRC Germplasm/Seed Stock";
-		Item germplasmDataSet = createDataSet(tairDataSourceItem, "TAIR Germplasm", version, url, description);
-		
-		
-		String versionPoly = "TAIR Polymorphism, October 2013";
-		String urlPoly = "https://www.arabidopsis.org/";
-		String descriptionPoly = "TAIR Polymorphism";
-		Item polyDataSet = createDataSet(tairDataSourceItem, "TAIR Polymorphism", versionPoly, urlPoly, descriptionPoly);
-		
-		String versionPheno = "TAIR Phenotypes, October 2013";
-		String urlPheno = "https://www.arabidopsis.org/";
-		String descriptionPheno = "TAIR Phenotypes";
-		Item phenoDataSet = createDataSet(tairDataSourceItem, "TAIR Phenotypes", versionPheno, urlPheno, descriptionPheno);
-		
-		String versionEcotypes = "TAIR Ecotypes, October 2013";
-		String urlEcotypes = "https://www.arabidopsis.org/";
-		String descriptionEcotypes = "TAIR Ecotypes";
-		Item ecotyTypesDataSet = createDataSet(tairDataSourceItem, "TAIR Ecotypes", versionPheno, urlPheno, descriptionPheno);
+            dataSetItem = StoreService.getService().createItem("DataSet");
+            dataSetItem.setAttribute("name", dataSetName);
+            dataSetItem.setReference("dataSource", datasourceRefId);
 
-	}
+            if (!StringUtils.isBlank(description)) {
+                dataSetItem.setAttribute("description", description);
+            }
 
-	private Item createDataSource(String dataSourceName) {
+            if (!StringUtils.isBlank(version)) {
+                dataSetItem.setAttribute("version", version);
+            }
 
-		Item dataSourceItem = null;
-		Exception exception = null;
-		ItemHolder itemHolder = null;
-		int itemId = -1;
+            if (!StringUtils.isBlank(url)) {
+                dataSetItem.setAttribute("url", url);
+            }
+        }
 
-		try {
+        try {
+            itemId = StoreService.getService().store(dataSetItem);
+        } catch (ObjectStoreException e) {
+            exception = e;
+        } catch (Exception e) {
+            exception = e;
+        } finally {
+            if (exception != null) {
+                LOG.error("Error storing item for dataset record:" + dataSetName);
+            } else {
+                LOG.debug("Target Item has been created. DataSet:" + dataSetItem);
 
-			dataSourceItem = StoreService.getService().createItem("DataSource");
-			dataSourceItem.setAttribute("name", dataSourceName);
-			itemId = StoreService.getService().store(dataSourceItem);
-
-		} catch (ObjectStoreException e) {
-			exception = e;
-		} catch (Exception e) {
-			exception = e;
-		} finally {
-
-			if (exception != null) {
-				log.error("Error storing item for datasource record:" + dataSourceName);
-			} else {
-				log.info("Target Item has been created. DataSource:" + dataSourceItem);
-
-				itemHolder = new ItemHolder(dataSourceItem, itemId);
-
-				if (itemHolder != null && itemId != -1) {
-
-					DataSourceService.addDataSourceItem(dataSourceName, itemHolder);
-
-				}
-
-			}
-
-		}
-
-		return dataSourceItem;
-
-	}
-
-	private Item createDataSet(Item dataSource, String dataSetName, String version, String url, String description) {
-
-		Item dataSetItem = null;
-		Exception exception = null;
-		ItemHolder itemHolder = null;
-		int itemId = -1;
-
-		if (dataSource != null && !StringUtils.isBlank(dataSetName)) {
-
-			String datasourceRefId = dataSource.getIdentifier();
-
-			dataSetItem = StoreService.getService().createItem("DataSet");
-			dataSetItem.setAttribute("name", dataSetName);
-			dataSetItem.setReference("dataSource", datasourceRefId);
-			
-			if (!StringUtils.isBlank(description)){
-				
-				dataSetItem.setAttribute("description", description);
-			}
-			
-			if (!StringUtils.isBlank(version)){
-				
-				dataSetItem.setAttribute("version", version);
-			}
-			
-			if (!StringUtils.isBlank(url)){
-				
-				dataSetItem.setAttribute("url", url);
-			}
-
-		}
-
-		try {
-
-			itemId = StoreService.getService().store(dataSetItem);
-
-		} catch (ObjectStoreException e) {
-			exception = e;
-		} catch (Exception e) {
-			exception = e;
-		} finally {
-
-			if (exception != null) {
-				log.error("Error storing item for dataset record:" + dataSetName);
-			} else {
-				log.info("Target Item has been created. DataSet:" + dataSetItem);
-
-				itemHolder = new ItemHolder(dataSetItem, itemId);
-
-				if (itemHolder != null && itemId != -1) {
-
-					DataSetService.addDataSetItem(dataSetName, itemHolder);
-
-				}
-
-			}
-
-		}
-
-		return dataSetItem;
-
-	}
-
+                itemHolder = new ItemHolder(dataSetItem, itemId);
+                if (itemHolder != null && itemId != -1) {
+                    DataSetService.addDataSetItem(dataSetName, itemHolder);
+                }
+            }
+        }
+        return dataSetItem;
+    }
 }
