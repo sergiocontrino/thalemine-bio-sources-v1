7848ab5ab4c715cb798d34907a63d88194af5d7d
Vivek Krishnakumar <vkrishna@jcvi.org>
2016-08-18 15:36:25 -0400

    + Adds Salk TDNA-seq GFF data source and associated loading code + Updates thalemine-static source with latest version numbers

---
+++ salk-tdnaseq-gff/main/src/org/intermine/bio/dataconversion/SalkTdnaseqGffGFF3RecordHandler.java
@@ -0,0 +1,67 @@
+package org.intermine.bio.dataconversion;
+
+/*
+ * Copyright (C) 2002-2016 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Iterator;
+
+import org.intermine.dataconversion.ItemWriter;
+import org.intermine.bio.io.gff3.GFF3Record;
+import org.intermine.metadata.Model;
+import org.intermine.metadata.StringUtil;
+import org.intermine.xml.full.Item;
+
+/**
+ * A converter/retriever for the SalkTdnaseqGff dataset via GFF files.
+ */
+
+public class SalkTdnaseqGffGFF3RecordHandler extends GFF3RecordHandler
+{
+
+    protected Map<String, Item> seqFeatMap = new HashMap<String, Item>();
+
+    /**
+     * Create a new SalkTdnaseqGffGFF3RecordHandler for the given data model.
+     * @param model the model for which items will be created
+     */
+    public SalkTdnaseqGffGFF3RecordHandler (Model model) {
+        super(model);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void process(GFF3Record record) {
+        // This method is called for every line of GFF3 file(s) being read.  Features and their
+        // locations are already created but not stored so you can make changes here.  Attributes
+        // are from the last column of the file are available in a map with the attribute name as
+        // the key.   For example:
+        //
+        //     Item feature = getFeature();
+        //     String symbol = record.getAttributes().get("symbol");
+        //     feature.setAttribute("symbol", symbol);
+        //
+        // Any new Items created can be stored by calling addItem().  For example:
+        //
+        //     String geneIdentifier = record.getAttributes().get("gene");
+        //     gene = createItem("Gene");
+        //     gene.setAttribute("primaryIdentifier", geneIdentifier);
+        //     addItem(gene);
+        //
+        // You should make sure that new Items you create are unique, i.e. by storing in a map by
+        // some identifier.
+
+    }
+
+}
